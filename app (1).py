49f42734789f0cdb0fa007d5b076eba317485cf1
Bright Ruengsit <brightsaiya@gmail.com>
2026-08-14 19:35:37 +0700

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -1,5 +1,5 @@
 import streamlit as st
-from PIL import Image
+from fastai.vision.core import PILImage
 from fastai.learner import load_learner
 import torch
 import torchvision.transforms as transforms
@@ -27,13 +27,13 @@ uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
     # Open the uploaded image
-    image = Image.open(uploaded_file)
+    image = PILImage.create(uploaded_file)
 
     # Display the image
     st.image(image, caption="Uploaded Image", use_container_width=True)
 
     # Make prediction using FastAI's model
-    pred, pred_idx, probs = model.predict(image)
+    prediction, _, probabilities = model.predict(image)
 
     # Display the prediction
     st.write(f"Prediction: **{pred}**")
