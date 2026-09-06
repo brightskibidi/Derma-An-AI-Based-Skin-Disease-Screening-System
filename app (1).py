1829aba4c7a3ac141442c063d6e91a187d292ef2
Bright Ruengsit <brightsaiya@gmail.com>
2026-09-06 12:56:00 +0700

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -164,18 +164,12 @@ disease_info = {
 # =========================================================
 @st.cache_resource
 def load_model():
-    model = load_learner(
-        "Skin_disease (1).pkl"
-    )
-    return model
-try:
-    model = load_model()
-except Exception as e:
-    st.error(
-        "Unable to load the AI model."
-    )
-    st.exception(e)
-    st.stop()
+    try:
+        model = load_learner("Skin_disease(1).pkl")
+        return model
+    except Exception as e:
+        st.error(f"Model loading failed: {type(e).__name__}: {e}")
+        raise
 # =========================================================
 # IMAGE UPLOADER
 # =========================================================
