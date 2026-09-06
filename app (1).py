57d87c6ae4fe7c2fccd2eff42b020885ae3ae05d
Bright Ruengsit <brightsaiya@gmail.com>
2026-09-06 13:53:33 +0700

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -170,6 +170,8 @@ def load_model():
     except Exception as e:
         st.error(f"Model loading failed: {type(e).__name__}: {e}")
         raise
+
+model = load_model()
 # =========================================================
 # IMAGE UPLOADER
 # =========================================================
